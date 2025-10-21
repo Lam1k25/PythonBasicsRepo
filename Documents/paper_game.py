b515993778d543fe7439e201ecb9d817e14d669c
Lam1k <brawlstars5534star@gmail.com>
2025-10-21 17:16:04 +0300

    initi

--- Documents/paper_game.py
+++ Documents/paper_game.py
@@ -1,8 +1,8 @@
 p_user: str = input("выберите: камень, ножницы, бумага: ")
-pc_user: str = "камень"
-if p_user == "бумага" and pc_user == "камень":
+pc_user: str = input()
+if p_user == "бумага" and pc_user == "камень" or p_user == "камень" and pc_user == "ножницы" or p_user == "ножницы" and pc_user == "бумага":
     print("Вы выиграли")
-elif p_user == "камень" and pc_user == "камень":
+elif p_user == "камень" and pc_user == "камень" or p_user == "ножницы" and pc_user == "ножницы" or p_user == "бумага" and pc_user == "бумага":
     print("Ничья")
 else:
     print('Вы проиграли')
