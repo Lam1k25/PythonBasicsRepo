db91b367b1de8e00c34bfa91183d75a80c78f209
Lam1k25 <brawlstars5534star@gmail.com>
2025-11-04 16:32:13 +0300

    Untitled-1.py

--- Documents/Untitled-1.py
+++ Documents/Untitled-1.py
@@ -1,13 +1,13 @@
 n: int = int(input())  
 m: int = int(input())  
   
-matrix =  []
+matrix: list[list[int] =  []
 for i in range(n):  
     row = list(map(int, input(f"Строка {i + 1}: ").split()))  
     matrix.append(row)  
   
 transposed = [[matrix[j][i] for j in range(n)] for i in range(m)]  
   
-#print("\nТранспонированная матрица:")  
 for row in transposed:  
     print(" ".join(map(str, row)))  
+
