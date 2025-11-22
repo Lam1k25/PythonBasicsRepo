36112ebf9cdf9818d7ad3cd8b3bde63891161ffc
Lam1k25 <brawlstars5534star@gmail.com>
2025-11-22 18:16:57 +0300

    Add files via upload

---
+++ make_report.py
@@ -0,0 +1,20 @@
+def make_report(title, **sections): 
+    print(f"=== Отчёт: {title} ===")  
+      
+    total = 0  
+      
+      
+    for section, value in sections.items():  
+        print(f"{section}: {value}")  
+        total += value  # Суммируем значения  
+      
+      
+    print(f"Общая сумма: {total}")  
+  
+ 
+make_report(  
+    "Продажи",  
+    Север=12000,  
+    Юг=8500,  
+    Запад=9000  
+)  
