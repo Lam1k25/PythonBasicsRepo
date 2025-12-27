a82bf74e3027db18c73ea1b9e51f5f7536834303
Lam1k25 <brawlstars5534star@gmail.com>
2025-12-27 20:35:50 +0300

    Add files via upload

--- homework_3.py
+++ homework_3.py
@@ -1,23 +1,28 @@
-# task 1
-m: int = int(input('введите число:'))
-n: int = int(input('введите число:'))
-for i in range(m, n + 1, + 1):
-    if i % 17 == 0 or i % 10 == 9 or i % 3 == 0 and i % 5 == 0:
-        print(i)
+def count_vowels(text: str) -> int:
+    vowels = "aeiou"
+    return sum(1 for char in text.lower() if char in vowels)
 
-# task 2
-n: int = int(input())
-for i in range(1, n + 1, + 1):
-    if  5 <= i <= 9:
-        continue
-    elif 17 <= i <= 37:
-        continue
-    elif 78 <= i <= 87:
-        continue
-    print(i)
 
-# task 3
-n: int = int(input())
-m = [n, n, n]
-for i in range(n):
-    print(m)
+import unittest
+
+
+class TestCountVowels(unittest.TestCase):
+
+    def test_regular_string(self):
+        self.assertEqual(count_vowels("hello"), 2)
+
+    def test_mixed_case(self):
+        self.assertEqual(count_vowels("HeLLo WoRLD"), 3)
+
+    def test_only_vowels(self):
+        self.assertEqual(count_vowels("aeiouAEIOU"), 10)
+
+    def test_no_vowels(self):
+        self.assertEqual(count_vowels("bcdfg"), 0)
+
+    def test_empty_string(self):
+        self.assertEqual(count_vowels(""), 0)
+
+
+if __name__ == "main":
+    unittest.main()
