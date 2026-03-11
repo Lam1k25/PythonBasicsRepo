6abd68b2c6116413153845dcbd893a07cee70a08
Lam1k25 <brawlstars5534star@gmail.com>
2026-03-11 21:20:16 +0300

    Update classwork.js

--- classwork.js
+++ classwork.js
@@ -164,11 +164,143 @@ else if((month == 11 && day >= 23) || (month == 12 && day < 22)){
     console.log("стрелец")
 }
 
+// 11.03.26
+
+// let - global, let  in {} - local,
+
+//part 1
+
+function myFunc(){
+    let text = "hello"; // вместо let можно const(область видимости внутри скобок)
+    console.log(text); //hello
+}
+console.log(text); //Ошибка
+//!!! внутри функции
+while(true){
+    let x = 10;
+    console.log(x); // 10
+}
+console.log(x); //ошибка
+//!!! внутри блока(любых фигурных скобок)
+
+for(var i = 0; i < 5; i++){
+    console.log(i)
+}
+// var - принимает как локальную, так и глобальню область
+
+//part 2
+
+let n = "3"
+n = Number(n);
+if(!isNaN (n) && n >= 0){
+    let sum = 0
+    for(let i = 1; i <= n; i++){
+        sum += i;
+    }
+    console.log(sum);
+
+}
+
+// part 3
+let n = 4
+let day = 5;
+let day_1 = 3;
+let day_2 = 4;
+let day_3 = 0;
+let day_4 = 2;
+let day_5 = 4;
+const hours = 24;
+const duration = 1.5;
+const travel = 1;
+const time = hours - duration - travel;
+let totalTraveltime = 2 * travel;
+let totalClassTime1 = duration * day_1;
+let totalClassTime2 = duration * day_2;
+let totalClassTime3 = duration * day_3;
+let totalClassTime4 = duration * day_4;
+let totalClassTime5= duration * day_5;
+let array = [totalClassTime1, totalClassTime2, totalClassTime3, totalClassTime4, totalClassTime5];
 
 
+if (array[day_1] < 0 || n > 5){
+    console.log("ошибка")
+
+}
+for(let days = 0; days < array.length; days++){
+    availebelTimeToSleep = hours - array[days] - totalTraveltime;
+    console.log(availebelTimeToSleep)
+}
+
+// part 4
+
+function sum(a = 0 , b = 0){
+    return a + b;
+}
+
+console.log(sum(5))
 
 
+function showmyname(){
+    console.log(this.name)
+}
 
+const person = {
+    name: "Bob",
+    showmyname: showmyname
+}
+
+person.showmyname()
+
+const sum = (a, b) => a + b;
+console.log(sum(3, 3))
+
+// part 5
+
+function findMax(a,b){
+    if (a > b){
+        return a;
+    }
+    else if (a == b){
+        return "числа равны"
+    }
+    else{
+        return b
+    }
+}
+
+
+console.log(findMax(3,5))
+
+//part 6
+
+function number(a){
+    if (a % 2 == 0){
+        console.log("число чётное")
+    }
+    else{
+        console.log("число нечётное")
+    }
+}
+
+console.log(number(6))
+
+let pred = 1
+let pred2 = 1
+let next = 2
+function Fibonachi(n){
+    if(n > 10){
+        console.log("ошибка")
+    }
+
+    for(i = 3; i < n; i++){
+        next = pred + pred2;
+        pred2 = pred;
+        pred = next;
+    }
+
+    return next
+}
+console.log(Fibonachi(10))
 
 
 
